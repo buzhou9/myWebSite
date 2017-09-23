9d9387d185d1794214eb6153886e43d62691128c
huang782351417 <782351417@qq.com>
2017-09-23 14:21:28 +0800

    首页提交

--- webSrc/scripts/components/cssAdapter.js
+++ webSrc/scripts/components/cssAdapter.js
@@ -1,5 +1,9 @@
 function cssAdapter(){
 	var width=window.innerWidth;
 	document.getElementsByTagName('html')[0].style.fontSize=width/568*9+'px';
-}
-export {cssAdapter};
+};
+function cssAdapterCom(){
+	var width=window.innerWidth;
+	document.getElementsByTagName('html')[0].style.fontSize=width/320*10+'px';
+};
+export {cssAdapter,cssAdapterCom};
